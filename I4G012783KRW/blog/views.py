c39d366c0ccdeea80540534dd8d60d1af9eed4ca
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-06-25 14:19:47 +0100

    Create blog app

---
+++ I4G012783KRW/blog/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
+from . models import Post
+# Create your views here.
+
+
+class PostCreateView(CreateView):
+  model = Post
+  fields = Post.objects.all()
+  success_url = reverse_lazy("blog:all")
+
+
+class PostUpdateView(UpdateView):
+  model = Post
+  fields = Post.objects.all()
+  success_url = reverse_lazy("blog:all")
+
+
+class PostDeleteView(DeleteView):
+  model = Post
+  fields = Post.objects.all()
+  success_url = reverse_lazy("blog:all")
+
+
+class PostListView(ListView):
+  model = Post
+  # fields = Post.objects.all()
+
+
+class PostDeleteView(DeleteView):
+  model = Post
+  # fields = Post.objects.all()
